b55c74bd04d8a2cdc6fbad859748d7b898d73c8f
Mojtaba Keshavarzi <mojtabakeshavarzi1998@gmail.com>
2023-02-14 11:34:13 +0330

    Hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaEye , FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
 
 function App() {
+
+  const [type , setType] = useState("password");
+  const [icon , setIcons] = useState(FaEyeSlash);
+
+  const clickHandler = () =>
+  {
+    if(type === "password") 
+    {
+      setType("text");
+      setIcons(FaEye);
+    }
+    else
+    {
+      setType("password");
+      setIcons(FaEyeSlash);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="input-fields">
+        <span onClick={clickHandler}>{icon}</span>
+        <input type={type} />
+      </div>
     </div>
   );
 }
